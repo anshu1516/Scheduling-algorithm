0cd555d36d4b6ba858b0a357bc92c57801b39de1
anshu1516 <63312827+anshu1516@users.noreply.github.com>
2020-04-09 20:18:48 +0530

    Add files via upload

---
+++ os1.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+struct student
+{
+    int StudentId;
+    int FoodTakenTime;
+    int WaitingTime;
+    int TurnAroundTime;
+};
+void get(struct student list[], int a);
+void show(struct student list[], int b);
+void scheduling(struct student list[], int c);
+void waitingTime(struct student list[], int d);
+void turnAroundTime(struct student list[], int e);
+
+int main()
+{
+    struct student data[30];
+    int p;
+    char f='A';
+    do
+	{
+    printf("Please enter the number of students who wants to eat in mess: ");
+    scanf("%d", &p);
+    get(data, p);
+    scheduling(data, p);
+    waitingTime(data,p);
+    turnAroundTime(data,p);
+    show(data, p);
+    printf("If you want to continue then press 'A': ");
+    scanf("%s",&f);
+    }
+	while(f=='A');
+    return 0;
+} 
+
+void get(struct student list[100], int a)
+{
+    int i;
+    for (i = 0; i < a; i++)
+    {
+        printf("\n\nEnter information of students %d", i + 1);
+        
+        printf("\nEnter Student id : ");
+        scanf("%d", &list[i].StudentId);
+
+        printf("Enter time taken for eating food in minutes: ");
+        scanf("%d", &list[i].FoodTakenTime);
+    } 
+}
+void scheduling(struct student list[100], int c)
+{
+    int i, j;
+    struct student temp;
+    
+    for (i = 0; i < c - 1; i++)
+    {
+        for (j = 0; j < (c - 1-i); j++)
+        {
+            if (list[j].FoodTakenTime < list[j + 1].FoodTakenTime)
+            {
